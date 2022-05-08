5786a3e8bdfa9a4f53a8d2ea5c0b076cb7a17c6c
Rishabh Tatiraju <me@rishabh.blog>
2022-05-08 12:45:12 +0530

    Added two rest APIs to get all stations and get station by ID

---
+++ bl.py
@@ -0,0 +1,28 @@
+import json
+
+
+def get_all_stations():
+    file = open('stations.json')
+    file_data = json.load(file)
+    file.close()
+    return {
+        'code': 200,
+        'result': file_data
+    }
+
+
+def get_station_by_id(id):
+    file = open('stations.json')
+    file_data = json.load(file)
+    file.close()
+    for station in file_data:
+        if station['stationId'] == id:
+            return {
+                'code': 200,
+                'result': station
+            }
+
+    return {
+        'code': 404,
+        'msg': 'No station with ID {} found'.format(id)
+    }
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+import bl
 
 app = Flask(__name__)
 
+
+@app.route('/station/<string:id>')
+def get_station(id):
+    if id == 'all':
+        result = bl.get_all_stations()
+    else:
+        result = bl.get_station_by_id(int(id))
+
+    return jsonify(result), result['code']
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=1875)

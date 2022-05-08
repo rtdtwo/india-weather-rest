6b0d0b44a5887a00847ed8ec572e477e60a5a734
Rishabh Tatiraju <me@rishabh.blog>
2022-05-08 21:20:22 +0530

    Added Procfile and modified main file

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+import bl
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import waitress
+import os
+
+RATE_LIMIT_DAILY = os.getenv('RATE_LIMIT_DAILY')
+RATE_LIMIT_HOURLY = os.getenv('RATE_LIMIT_HOURLY')
+
+app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["{} per day".format(RATE_LIMIT_DAILY), "{} per hour".format(RATE_LIMIT_HOURLY)]
+)
+
+@app.route('/station/<string:id>')
+def get_station(id):
+    if id == 'all':
+        result = bl.get_all_stations()
+    else:
+        result = bl.get_station_by_id(int(id))
+
+    return jsonify(result), result['code']
+
+
+@app.route('/weather/<int:id>')
+def get_station_weather(id):
+    result = bl.get_station_weather(id)
+    return jsonify(result), result['code']
+
+
+if __name__ == '__main__':
+    # Uncomment the waitress line and 
+    # comment the flask (app.run) line 
+    # on a production environment, 
+    # or vice versa.
+
+    waitress.serve(host='0.0.0.0', port=1875)
+    # app.run(host='0.0.0.0', port=1875)
